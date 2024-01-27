477c3107e37949eb5b3df63af2b5d1e29f39f7e9
Muhamad Sumair Shaukat <sumairch224@gmail.com>
2024-01-27 23:03:31 +0500

    s3 added

---
+++ index.js
@@ -0,0 +1,23 @@
+ const {S3Client, GetObjectCommand} = require('@aws-sdk/client-s3');
+  const getSignedUrl = require('@aws-sdk/s3-request-presigner')
+ const s3client = new S3Client({
+    region: '',
+    Credentials: {
+    accesskeyId: '',
+    secretAccessKey: ''
+
+    }
+ })
+
+ const newObject = async (key) => {
+    const command = new GetObjectCommand({
+        Bucket: "",
+        Key: key
+    })
+    const url =  await s3Client(command, s3client);
+    return url
+ } 
+ async function inti ( ) {
+    console.log('get signed url from aws', newObject('graphql.jpeg'))
+ }
+ inti()
